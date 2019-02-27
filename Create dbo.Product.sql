0d7342b4c627d3492a636ead069cfd11d611572e
KeshavDeekonda <46499786+KeshavDeekonda@users.noreply.github.com>
2019-02-26 21:26:18 -0500

    Add files via upload

---
+++ Create dbo.Product.sql
@@ -0,0 +1,19 @@
+﻿USE [test]
+GO
+
+/****** Object: Table [dbo].[Product] Script Date: 2/26/2019 9:19:21 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Product] (
+    [ProductID]           VARCHAR (50) NOT NULL,
+    [ProductName]         VARCHAR (50) NULL,
+    [ProductBatch]        VARCHAR (50) NULL,
+    [ProductSellingPrice] VARCHAR (50) NULL,
+    [DepartmentID]        VARCHAR (50) NULL
+);
+
+
---
+++ Create dbo.Sales.sql
@@ -0,0 +1,19 @@
+﻿USE [test]
+GO
+
+/****** Object: Table [dbo].[Sales] Script Date: 2/26/2019 9:21:40 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Sales] (
+    [DepartmentID]   VARCHAR (50) NOT NULL,
+    [DepartmentName] VARCHAR (50) NULL,
+    [SalesQuantity]  VARCHAR (50) NULL,
+    [SalesValue]     VARCHAR (50) NULL,
+    [ProductID]      VARCHAR (50) NULL
+);
+
+
